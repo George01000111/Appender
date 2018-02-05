f74bed4954e5874a44be86421afa0ad50c8a32ec
georgesoft07 <george_soft_01@hotmail.com>
2018-02-05 17:33:59 -0500

    first commit

---
+++ ConsoleApplication1/SQLScript/Query.sql
@@ -0,0 +1,16 @@
+[macedonia]
+
+SELECT * FROM Log
+
+[macedonia]
+INSERT INTO Log ([Date],[Thread],[Level],[Logger],[Message],[Exception]
+
+
+
+CREATE TABLE Log
+([Date] DateTime,
+Thread NVARCHAR(MAX),
+[Level] NVARCHAR(MAX),
+[Logger] NVARCHAR(MAX),
+[Message] NVARCHAR(MAX),
+[Exception] NVARCHAR(MAX))
