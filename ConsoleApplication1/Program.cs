4b81f9bbceb811f3784cd23fb9edebe5ac3c852a
georgesoft07 <george_soft_01@hotmail.com>
2018-02-05 17:26:44 -0500

    first commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+
+        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+
+        static void Main(string[] args)
+        {
+            log4net.Config.XmlConfigurator.Configure();
+            log.Info("Estoy registrando este evento");
+            log.Error("ERROR!");
+            log.Debug("Se ha iniciado el servicio de base de datos");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,30 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(WebForm1));
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            throw new Exception("este un error al iniciar la pagina");
+
+        }
+        void Page_Error(object sender,EventArgs e)
+        {
+            Exception exc = Server.GetLastError();
+            log.Error(exc.Message,exc);
+            Server.ClearError();
+        }
+
+
+    }
+}
